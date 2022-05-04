fd5914d177b1e3f183b1cc58af7b44c2383da442
S Abhinav <60837594+sabhinav3@users.noreply.github.com>
2022-05-05 00:50:20 +0530

    created the data services and data model dart file

--- lib/services/data_services.dart
+++ lib/services/data_services.dart
@@ -1,19 +1,24 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:int_appone/model/data_model.dart';
 
 class DataServices {
   String baseUrl = "http://mark.bslmeiyu.com/api";
-  getInfo() async {
+  Future<List<DataModel>> getInfo() async {
     var apiUrl = '/getPlaces';
     http.Response res = await http.get(Uri.parse(baseUrl + apiUrl));
 
     try {
       if (res.statusCode == 200) {
         List<dynamic> list = json.decode(res.body);
+        return list.map((e) => DataModel.fromJson(e)).toList();
+      } else {
+        return <DataModel>[];
       }
     } catch (e) {
       print(e);
+      return <DataModel>[];
     }
   }
 }
